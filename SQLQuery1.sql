d40adf6ef12dfb6333c936d178a1146525d7f6e6
Евгений Ровенский <116351368+yirovensky@users.noreply.github.com>
2024-02-01 14:47:54 +0300

    Update SQLQuery1.sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,7 +1,7 @@
 ﻿INSERT INTO employees (last_name, first_name) VALUES ('Иванов', 'Иван');
 INSERT INTO employees (last_name, first_name) VALUES ('Петров', 'Петр');
 INSERT INTO employees (last_name, first_name) VALUES ('Сидоров', 'Сидор');
-INSERT INTO employees (last_name, first_name) VALUES ('Козлов', 'Козел');
+INSERT INTO employees (last_name, first_name) VALUES ('Егоров', 'Егор');
 INSERT INTO employees (last_name, first_name) VALUES ('Федоров', 'Федор');
 
 INSERT INTO reasons (name) VALUES ('Отпуск');
