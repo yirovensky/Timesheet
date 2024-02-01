79fb04a3528c7b5dd4a66388d1e5ca9580c74364
Евгений Ровенский <116351368+yirovensky@users.noreply.github.com>
2024-02-01 14:47:03 +0300

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,9 @@
+﻿INSERT INTO employees (last_name, first_name) VALUES ('Иванов', 'Иван');
+INSERT INTO employees (last_name, first_name) VALUES ('Петров', 'Петр');
+INSERT INTO employees (last_name, first_name) VALUES ('Сидоров', 'Сидор');
+INSERT INTO employees (last_name, first_name) VALUES ('Козлов', 'Козел');
+INSERT INTO employees (last_name, first_name) VALUES ('Федоров', 'Федор');
+
+INSERT INTO reasons (name) VALUES ('Отпуск');
+INSERT INTO reasons (name) VALUES ('Больничный');
+INSERT INTO reasons (name) VALUES ('Прогул');
